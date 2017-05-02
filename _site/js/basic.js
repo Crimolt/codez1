09114cbf4f4b499a02434c23918ee54ab999eb6a
Crimolt <leviplj@gmail.com>
2017-05-02 14:21:54 -0300

    Removendo _site e adicionando .gitignore

--- _site/js/basic.js
+++
@@ -1,8 +0,0 @@
-console.log("ca");
-window.addEventListener('scroll', function() {  
-  if (window.scrollY > 0) {
-    document.querySelector('body').classList.add("scrolled");
-  } else {
-    document.querySelector('body').classList.remove("scrolled");
-  }
-});
