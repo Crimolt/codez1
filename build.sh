dbf15a01b780aff4806c67bd0f58d2a04ed870dd
Crimolt <leviplj@gmail.com>
2017-06-06 22:52:58 -0300

    ..

--- build.sh
+++ build.sh
@@ -46,11 +46,11 @@ function deploy {
 	git init
 	git config --global user.name "Crimolt"
     git config --global user.email leviplj@gmail.com
-	git remote add origin https://github.com/Crimolt/codez1.git
+	git remote add cdz https://github.com/Crimolt/codez1.git
 	git add .
 	git status
 	git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
-	git push $DEPLOY_REPO master:gh-pages
+	git push cdz master:gh-pages --force
 }
 
 main
